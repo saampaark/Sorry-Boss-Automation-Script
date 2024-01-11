694ba7beb7b9805cbe314238acd28806b010503f
Sam Park <sampark@v1040-wn-rt-b-72-107.campus-dynamic.uwaterloo.ca>
2024-01-11 17:00:16 -0500

    first commit

---
+++ script.py
@@ -0,0 +1,58 @@
+import requests
+import smtplib
+from email.message import EmailMessage
+
+# API Key
+api_key = "AIzaSyAOv_VFZKT_vuAKvQvQPRYJn8g4MwZzDds"
+
+# Home Address Input
+home = input("Enter home address:\n")
+
+# Work Address Input
+work = input("Enter work address:\n")
+
+# Base URL
+url = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&"
+
+# Get Response
+r = requests.get(url + "origins=" + home + "&destinations=" + work + "&key=" + "AIzaSyAOv_VFZKT_vuAKvQvQPRYJn8g4MwZzDds")
+
+# Return Time as Text and as Seconds
+time = r.json()["rows"][0]["elements"][0]["duration"]["text"]
+seconds = r.json()["rows"][0]["elements"][0]["duration"]["value"]
+
+# Print Total Travel Time
+print("\nThe total time from home to work is", time)
+
+# Check if Time > 1 Hour
+if (seconds > 3600):
+
+    # email constraints
+    sender = 'sam.park00000@gmail.com'
+    recipient = input("Enter receiving email:\n")
+    subject = "Sick Day"
+    message = """
+    Hi team, 
+    
+    Sorry for the inconvenience, but I can't make it to work today.
+    
+    Thanks, 
+    Sam Park"""
+
+    # Email 
+    em = f"Subject: {subject}\n\n{message}"
+
+    # Create and Start SMTP Server
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+
+    # Authentication
+    server.login(sender, "mwedalabsuucoilb")
+
+    # Sending email
+    server.sendmail(sender, recipient, em)
+
+    # Success Message 
+    print("\nSuccessfully sent a sick-day email to", recipient, "since the travel time was too long.")
+else:
+    print("\nEmail was not sent. The commute is short, lets get going to work!")
